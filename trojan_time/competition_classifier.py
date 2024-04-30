99af660574f5fc33aeb908a91103e591c6e559ff
Jerry Han <jhan66260@gmail.com>
2024-04-30 18:21:08 -0400

    hooked up classifier pipeline

--- trojan_time/competition_classifier.py
+++ trojan_time/competition_classifier.py
@@ -23,6 +23,7 @@ from sklearn import preprocessing
 from sklearn.metrics import roc_auc_score
 
 from competition_model_data import ModelBasePaths, ModelData
+from classifier_bin import xgb_classifier
 
 def seed_everything(seed = 42):
     random.seed(seed)
@@ -143,33 +144,15 @@ def run_model_tests(feature, labels, model_list, thresholds = None, calc_thresho
             "thresholds": thresholds[best_ind]
             }
 
-def train_xgboost(models: List[ModelData]):
+
+def featurize(models: List[ModelData]): 
     CLASSES = 5 # FIXME don't hardcode this
     n_classes = CLASSES
-
-    TRAIN_TEST_SPLIT = 0.8
-
-    ######################################################################
-    ######################################################################
-
     fv_list = [x.fv for x in models]
     gt_list = [x.label for x in models]
 
-    # PSF feature shape = N*2*m*w*h*L*C
-    #   n: number of models
-    #   2: logits and confidence
-    #   m: number of input images
-    #   w: width of the feature map
-    #   h: height of the feature map
-    #   L: number of stimulation levels
-    #   C: number of classes
-
     psf_feature=torch.cat([fv_list[i]['psf_feature_pos'].unsqueeze(0) for i in range(len(fv_list))])
-
-    # TOPO feature shape = N*12 where 12 is the total number of topological feature from dim0 and dim1
     topo_feature = torch.cat([fv_list[i]['topo_feature_pos'].unsqueeze(0) for i in range(len(fv_list))])
-
-
     topo_feature[np.where(topo_feature==np.Inf)]=1
     n, _, nEx, fnW, fnH, nStim, C = psf_feature.shape
     psf_feature_dat=psf_feature.reshape(n, 2, -1, nStim, C)
@@ -179,13 +162,21 @@ def train_xgboost(models: List[ModelData]):
     psf_topk_max=psf_feature_dat.topk(k=min(3, n_classes), dim=3)[0].mean(2).max(2)[0].view(len(gt_list), -1)
     psf_feature_dat=torch.cat([psf_diff_max, psf_med_max, psf_std_max, psf_topk_max], dim=1)
 
-    # dat=torch.cat([psf_feature_dat, topo_feature.view(topo_feature.shape[0], -1)], dim=1)
-    # dat = topo_feature.view(topo_feature.shape[0], -1)
-
-    dat = psf_feature_dat
-
+    dat=torch.cat([psf_feature_dat, topo_feature.view(topo_feature.shape[0], -1)], dim=1)
     dat=preprocessing.scale(dat)
     gt_list=torch.tensor(gt_list)
+
+    return {
+        "features": np.array(dat),
+        "labels": np.array(gt_list) 
+    }
+
+
+def train_xgboost(models: List[ModelData]):
+    TRAIN_TEST_SPLIT = 0.8
+    _x = featurize(models)
+    dat = _x['features']
+    gt_list = _x['labels']
 
     N = len(gt_list)
     n_train = int(TRAIN_TEST_SPLIT * N)
@@ -197,9 +188,61 @@ def train_xgboost(models: List[ModelData]):
     gt_train, gt_test = gt_list[train_ind], gt_list[test_ind]
 
     log.info("beginning xgboost training")
-
     best_model_list = run_crossval_xgb(np.array(feature_train), np.array(gt_train))
 
+    """
+    # loop through a grid search of thresholds T and b
+    train_results = []
+    test_results = []
+    for T in np.linspace(0.01, 0.9, 20):
+        for b in np.linspace(0.000001, 0.3, 20):
+
+            # testing!
+            feature = feature_test
+            labels = np.array(gt_test)
+            dtest = xgb.DMatrix(np.array(feature), label=labels)
+            y_pred = 0
+            for i in range(len(best_model_list['models'])):
+                best_bst=best_model_list['models'][i]
+                weight=best_model_list['weight'][i]/sum(best_model_list['weight'])
+                y_pred += best_bst.predict(dtest)*weight
+
+            y_pred = y_pred / len(best_model_list)
+            # T, b=best_model_list['threshold']
+            y_pred=torch.sigmoid(b*(torch.tensor(y_pred)-T)).numpy()
+            acc_test = np.sum((y_pred >= 0.5)==labels)/len(y_pred)
+            auc_test = roc_auc_score(labels, y_pred)
+            ce_test = np.sum(-(labels * np.log(y_pred) + (1 - labels) * np.log(1 - y_pred))) / len(y_pred)
+
+            # also test on the training set
+            feature = feature_train
+            labels = np.array(gt_train)
+            dtest = xgb.DMatrix(np.array(feature), label=labels)
+            y_pred = 0
+            for i in range(len(best_model_list['models'])):
+                best_bst=best_model_list['models'][i]
+                weight=best_model_list['weight'][i]/sum(best_model_list['weight'])
+                y_pred += best_bst.predict(dtest)*weight
+
+            y_pred = y_pred / len(best_model_list)
+            # T, b=best_model_list['threshold']
+            y_pred=torch.sigmoid(b*(torch.tensor(y_pred)-T)).numpy()
+            acc_train = np.sum((y_pred >= 0.5)==labels)/len(y_pred)
+            auc_train = roc_auc_score(labels, y_pred)
+            ce_train = np.sum(-(labels * np.log(y_pred) + (1 - labels) * np.log(1 - y_pred))) / len(y_pred)
+
+            # logging.info(f"train acc: {acc_train:.4f}, train auc: {auc_train:.4f}, train ce: {ce_train:.4f}")
+            # logging.info(f"test acc: {acc_test:.4f}, test auc: {auc_test:.4f}, test ce: {ce_test:.4f}")
+
+            train_results.append((T, b, acc_train, auc_train, ce_train))
+            test_results.append((T, b, acc_test, auc_test, ce_test))
+
+    test_results = sorted(test_results, key=lambda x: x[2], reverse=True)
+    train_results = sorted(train_results, key=lambda x: x[2], reverse=True)
+
+    print(test_results, "\n\n")
+    print(train_results)
+    """
 
     train_results = run_model_tests(feature_train, gt_train, best_model_list, calc_thresholds=True)
     test_results = run_model_tests(feature_test, gt_test, best_model_list, thresholds=train_results['thresholds'])
@@ -210,16 +253,13 @@ def train_xgboost(models: List[ModelData]):
     print("test", test_results)
     # print("manual_restuls", manual_restuls)
 
+
 if __name__ == "__main__":
 
-
     device = torch.device('mps')
-    # device = torch.device('mps')
-    # device = torch.device('cpu')
 
     # TODO update this to ur device
-    # root = "/Users/huxley/dataset_storage/snn_tda_mats/LENET_MODELS/competition_dataset"
-    root = "/home/dataset_storage/TopoTrojDetect/competition_dataset"
+    root = "/home/jerryhan/Documents/data"
     models_dir = join(root, "all_models")
     cache_dir = join(root, "calculated_features_cache")
 
@@ -249,9 +289,28 @@ if __name__ == "__main__":
     np.random.shuffle(models)
 
     print(len(models))
+    print("Featurizing...")
+    TRAIN_TEST_SPLIT = 0.8
+    _x = featurize(models)
+    dat = _x['features']
+    gt_list = _x['labels']
 
+    N = len(gt_list)
+    n_train = int(TRAIN_TEST_SPLIT * N)
+    ind_reshuffle = np.random.choice(list(range(N)), N, replace=False)
+    train_ind = ind_reshuffle[:n_train]
+    test_ind = ind_reshuffle[n_train:]
 
-    train_xgboost(models)
+    feature_train, feature_test = dat[train_ind], dat[test_ind]
+    gt_train, gt_test = gt_list[train_ind], gt_list[test_ind]
+
+    x = xgb_classifier(features = {'train': feature_train, 'test': feature_test}, \
+                          labels = {'train': gt_train, 'test': gt_test})
+    print("Training...")
+    x.train()
+    print("Evaluating")
+    x.test()
+    # train_xgboost(models)
 
 
 
