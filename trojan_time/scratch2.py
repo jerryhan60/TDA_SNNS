2cf419a6a35ec56c350063b133700b92c594e562
mb6611 <mb6611@princeton.edu>
2024-05-04 23:14:09 -0400

    betti curve plotter

--- trojan_time/scratch2.py
+++ trojan_time/scratch2.py
@@ -210,6 +210,87 @@ def amplitude_feature_from_diagram_list(
 #         )
 # print(persistence_image)
 
+def diagram_list_to_betti_curves(
+        diagram_list: List[np.ndarray], n_bins: int = 100, n_jobs: int | None = None):
+
+    """
+
+    Input:
+
+    diagram_list - List[ List[ np.ndarray, np.ndarray] ] - list of persistance diagrams corresponding to
+    a single model. A persistence diagram is a list of np.ndarrays of shape (num_features, 2), where the ith
+    list is the ith homology group. Assumes two homology groups, H0 and H1.
+
+    n_bins - int - number of bins to use for the betti curve
+
+    n_jobs - int | None - number of jobs to run in parallel. None means 1 job.
+
+    Output:
+
+    (betti_curve, fit_betti_curve)
+
+    betti_curve - np.ndarray - (num_features, 2) betti curve
+
+    fit_betti_curve - a gtda.diagrams.BettiCurve object fit to the input diagrams
+
+    """
+
+    betti_curve = gtda.diagrams.BettiCurve(n_bins=n_bins, n_jobs=n_jobs)
+    # result = betti_curve.fit_transform(diagram, n_bins=n_bins, n_jobs=n_jobs)
+
+    # preprocessing
+    bdq_ordered_diagram_list = []
+    for diagram in diagram_list:
+        ones_H0 = np.ones((len(diagram[0]), 1))
+        ones_H1 = np.ones((len(diagram[1]), 1))
+        H0 = np.hstack((np.array(diagram[0]), ones_H0 * 0))
+        H1 = np.hstack((np.array(diagram[1]), ones_H1 * 1))
+
+        combined_array = np.array([np.vstack((H0, H1))])
+
+        result = betti_curve.fit_transform(combined_array)
+        # plot = betti_curve.plot(result)
+        # plot.write_image("betti_curve.png")
+        # exit()
+        # if fit_params is not None:
+            # metric = amplitude.fit_transform(X=combined_array, **fit_params)[0]
+        # else:
+            # metric = amplitude.fit_transform(X=combined_array)[0]
+
+        bdq_ordered_diagram_list.append(result)
+
+    return bdq_ordered_diagram_list, betti_curve
+
+def plot_betti_curve(betti_curve: List[np.ndarray], fitBettiCurve: gtda.diagrams.BettiCurve):
+
+    """
+    Input:
+
+    betti_curve - np.ndarray - (num_features, 2) betti curve
+
+    fitBettiCurve - a gtda.diagrams.BettiCurve object fit to the input diagrams (used for plotting),
+    obtained from diagram_list_to_betti_curves
+
+    Output:
+
+    plot - plotly.graph_objs..Figure - plot of the betti curve
+
+    optionally, save the plot as an image using plot.write_image("betti_curve.png")
+
+    """
+    plot = fitBettiCurve.plot(betti_curve)
+
+    # uncomment to save the plot
+    plot.write_image("betti_curve.png")
+
+    return plot
+
+# curves, fitBettiCurve = diagram_list_to_betti_curves(ph_list[0], n_bins=100, n_jobs=None)
+# plot_betti_curve(betti_curve=curves[0], fitBettiCurve=fitBettiCurve)
+
+
+
+
 def plot_persistence_image(diagram: np.ndarray, homology_group: int = 0):
 
     """
@@ -235,12 +316,11 @@ def plot_persistence_image(diagram: np.ndarray, homology_group: int = 0):
 # plot_persistence_image(diagram[0], homology_group=1)
 
 def wasserstein_distance_from_diagram_list(diagram_list: List[List[np.ndarray]]):
-
     """
 
     Input:
 
-    diagram_list - List[ List[ np.ndarray, np.ndarray] ] - list of persistance diagrams
+        diagram_list - List[ List[ np.ndarray, np.ndarray] ] - list of persistance diagrams
     corresponding to a single model. A persistence diagram is a list of np.ndarrays
     of shape (num_features, 2), where the ith list is the ith homology group. Assumes
     two homology groups, H0 and H1.
@@ -267,6 +347,7 @@ def wasserstein_distance_from_diagram_list(diagram_list: List[List[np.ndarray]])
         bdq_ordered_diagram_list.append(wasserstein_distance)
 
     return bdq_ordered_diagram_list
+
 
 # ph_list = [x.PH_list for x in models]
 # print(ph_list[0][0])
